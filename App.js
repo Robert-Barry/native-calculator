c1836a3365f2f1c421d5fd7c0d1a594c30cd6ada
Robert Barry <rb4080@gmail.com>
2026-01-15 14:05:38 -0500

    Add basic elements for a graphical calulator.

--- App.js
+++ App.js
@@ -1,20 +1,88 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StyleSheet,
+  Text, 
+  View,
+  TextInput,
+  Pressable
+} from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.heading}>React Native Calculator</Text>
+      <TextInput
+        style={styles.output}
+        editable={false}
+      />
+      <View>
+        <View style={styles.row}>
+          <Button  data='<' />
+          <Button  data='AC' />
+          <Button  data='%' />
+          <Button  data='/' />
+        </View>
+        <View style={styles.row}>
+          <Button  data='7' />
+          <Button  data='8' />
+          <Button  data='9' />
+          <Button  data='x' />
+        </View>
+        <View style={styles.row}>
+          <Button  data='4' />
+          <Button  data='5' />
+          <Button  data='6' />
+          <Button  data='-' />
+        </View>
+        <View style={styles.row}>
+          <Button  data='1' />
+          <Button  data='2' />
+          <Button  data='3' />
+          <Button  data='+' />
+        </View>
+        <View style={styles.row}>
+          <Button  data='+/-' />
+          <Button  data='0' />
+          <Button  data='.' />
+          <Button  data='=' />
+        </View>
+      </View>
     </View>
   );
 }
 
+const Button = (props) => {
+  return (
+    <Pressable style={styles.button}>
+      <Text>{props.data}</Text>
+    </Pressable>
+  )
+}
+
+export default App;
+
 const styles = StyleSheet.create({
   container: {
+    marginTop: 50,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    //backgroundColor: '#fff',
+    //alignItems: 'center',
+    //justifyContent: 'center',
   },
+  heading: {
+    fontWeight: "bold"  ,
+    fontSize: 24
+  },
+  output: {
+    backgroundColor: '#eee',
+    height: 28,
+  },
+  button: {
+    margin: 2,
+    backgroundColor: '#ddd',
+    width: 30
+  },
+  row: {
+    flexDirection: 'row'
+  }
 });
